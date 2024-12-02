6b8e83b2160bc00e1a3086c42f39ba456573806c
divineProjects <divinejigu@gmail.com>
2024-12-02 23:34:50 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongodb = require("./data/database");
+const bodyParser = require("body-parser");
+
+const PORT = 8081;
+const port = process.env.PORT || PORT;
+
+app.use(bodyParser.json());
+// CORS middleware to allow cross-origin requests
+a
+// Use routes defined in routes/index.js
+app.use("/", require("./routes"));
+
+// Initialize the database
+mongodb.initDb((err) => {
+    if (err) {
+      console.error("Failed to connect to MongoDB", err);
+      process.exit(1); // Exit the process if the DB connection fails
+    }
+  
+    console.log("Connected to MongoDB ...");
+    // Start the server once the DB is connected
+    app.listen(PORT, () => {
+      console.log(`Server is running on PORT ${PORT}`);
+    });
+  });
+  
